9304408039ee775ff1ce65c8ff4bb6c8c23d5aa0
Young sun <lsun@live.cn>
2018-06-19 18:09:03 +0800

    1：添加长按删除

--- app/src/main/java/net/lsun/library/RootActivity.java
+++ app/src/main/java/net/lsun/library/RootActivity.java
@@ -1,17 +1,22 @@
 package net.lsun.library;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -33,11 +38,12 @@ public class RootActivity extends AppCompatActivity {
         ListView userList = findViewById(R.id.root_user_list);
         Button addUser = findViewById(R.id.root_add);
 
-        ArrayList<String> arrayList = new ArrayList<>();
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.text_view, arrayList);
+        final ArrayList<String> arrayList = new ArrayList<>();
+        final ArrayList<String> arrayListID = new ArrayList<>();
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.text_view, arrayList);
         userList.setAdapter(arrayAdapter);
 
-        SQLite sqLite = new SQLite(this, "userList", null, 1);
+        final SQLite sqLite = new SQLite(this, "userList", null, 1);
         sqLiteDatabase = sqLite.getReadableDatabase();
         final Cursor cursor = sqLiteDatabase.query("userList", null,null,null,null,null,null);
 
@@ -53,6 +59,7 @@ public class RootActivity extends AppCompatActivity {
                     break;
             }
             arrayList.add("  用户："+cursor.getString(1)+"\n  密码："+cursor.getString(2)+"\n  权限："+su);
+            arrayListID.add(cursor.getString(cursor.getColumnIndex("user")));
         }
 
         addUser.setOnClickListener(new View.OnClickListener() {
@@ -63,8 +70,35 @@ public class RootActivity extends AppCompatActivity {
             }
         });
 
-    }
+        userList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                final String[] items = {"确定","取消"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(RootActivity.this);
+                builder.setTitle("删除 " + arrayListID.get(position) + "？").setItems(items, new DialogInterface.OnClickListener() {
 
+                    //弹窗点击
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        switch (which) {
+                            case 0:
+                                //单引号转字符串
+                                String sql = "delete from userList where user='" + arrayListID.get(position)+"'";
+                                sqLiteDatabase.execSQL(sql);
+                                arrayListID.remove(position);
+                                arrayList.remove(position);
+                                arrayAdapter.notifyDataSetChanged();
+                                break;
+                            case 1:
+                                break;
+                        }
+                    }
+                }).create().show();
+                return false;
+            }
+        });
+    }
     //返回键功能
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
